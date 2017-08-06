2fdcc65083f3d5401c7eb5a72a58e8bee8c0f59f
young <2388925311@qq.com>
2017-08-07 00:55:38 +0800

    用angular cli创建一个angular项目

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+
+//import {MaterialModule} from '@angular/material'; //引入 angular material2所有模块
+
+// import {MdButtonModule,MdCheckboxModule} from '@angular/material';//引入需要使用的模块
+
+
+//创建单独的NgModule模块引入需要使用的模块，方便在任意地方使用组件
+// @NgModule({
+//   imports:[MdButtonModule,MdCheckboxModule],
+//   exports:[MdButtonModule,MdCheckboxModule],
+// })
+// export class MyOwnCustomMaterialModule{ }  /*********/
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
